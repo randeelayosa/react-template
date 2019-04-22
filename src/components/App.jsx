83ec074c2fb507277f2e0d79df6c87381a458f58
Student <example@example.com>
2019-04-22 13:15:04 -0700

    Add a constructor, proptypes, refs, and stateful classes.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
-import { Switch, Route } from 'react-router-dom';
+import Header from './Header';
+import TicketList from './TicketList';
+import NewTicketControl from './NewTicketControl';
 import Error404 from './Error404';
-import Header from './Header'
-import TicketList from './TicketList'
+import { Switch, Route } from 'react-router-dom';
 import MyStyledComponent from './MyStyledComponent'
-import NewTicketForm from './NewTicketForm';
 
 function App(){
   return (
@@ -12,7 +12,7 @@ function App(){
       <Header/>
       <Switch>
         <Route exact path='/' component={TicketList} />
-        <Route path='/newticket' component={NewTicketForm} />
+        <Route path='/newticket' component={NewTicketControl} />
         <Route component={Error404} />
       </Switch>
     </div>
---
+++ src/components/NewTicketControl.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import NewTicketForm from './NewTicketForm';
+import ConfirmationQuestions from './ConfirmationQuestions';
+
+class NewTicketControl extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      formVisibleOnPage: false
+    };
+    this.handleTroubleshootingConfirmation = this.handleTroubleshootingConfirmation.bind(this);
+  }
+
+  handleTroubleshootingConfirmation(){
+    this.setState({formVisibleOnPage: true});
+  }
+
+  render(){
+    let currentlyVisibleContent = null;
+    if (this.state.formVisibleOnPage){
+      currentlyVisibleContent = <NewTicketForm />;
+    } else {
+      currentlyVisibleContent = <ConfirmationQuestions onTroubleshootingConfirmation={this.handleTroubleshootingConfirmation} />;
+    }
+    return (
+      <div>
+        {currentlyVisibleContent}
+      </div>
+    );
+  }
+}
+
+export default NewTicketControl;
